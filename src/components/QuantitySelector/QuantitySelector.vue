d64d43a47ef9d08b79987bf51aa3081a50aabd38
Shreerang Patwardhan <shreerang.patwardhan@macys.com>
2018-11-01 21:08:19 -0700

    Added editable and non-editable count option to Quantity selector

--- src/components/QuantitySelector/QuantitySelector.vue
+++ src/components/QuantitySelector/QuantitySelector.vue
@@ -7,7 +7,8 @@
                 </g>
             </svg>
         </div>
-        <input type="text" v-model="countData" @blur="adjustCount" />
+        <input v-if="isCountEditable" type="text" v-model="countData" @blur="adjustCount" />
+        <div v-else>{{countData}}</div>
         <div @click="increment">
             <svg viewBox="0 0 24 24" :width="iconDimensions" :height="iconDimensions">
                 <g>
@@ -22,104 +23,107 @@
 export default {
 	name: 'quantity-selector',
 	props: {
-        count: {
-            type: Number,
-            default: 1
-        }, // Makes sense to have default product count value
-        maxCount: {
-            type: Number,
-            default: 6
-        }, // maxCount makes sense when you have a restriction on the max quantity for a product
-        iconDimensions: {
-            type: Number,
-            default: 15
-        },
-        minusIconFillColor: {
-            type: String,
-            color: '#000',
-        },
-        plusIconFillColor: {
-            type: String,
-            color: '#000',
-        },
-    },
-    data() {
-        return {
-            countData: this.count
-        }
-    },
-    computed: {
-        minusIconColor: function(){
-            return this.countData === this.count ? '#CCC' : this.minusIconFillColor
-        },
-        plusIconColor: function(){
-            return this.countData === this.maxCount ? '#CCC' : this.plusIconFillColor
-        },
-    },
-    methods: {
-        decrement: function() {
-            if(this.countData > this.count){
-                this.countData -= 1
-            }
-        },
-        increment: function() {
-            if(this.countData < this.maxCount){
-                this.countData += 1
-            }
-        },
-        adjustCount: function() {
-            if(isNaN(Number(this.countData)) && (this.countData > this.maxCount)){
-                this.countData = this.maxCount
-            } else if(isNaN(Number(this.countData)) && (this.countData < this.count)) {
-                this.countData = this.count
-            } else {
-                this.countData = this.count
-            }
-        }
-    }
+		count: {
+			type: Number,
+			default: 1,
+		}, // Makes sense to have default product count value
+		maxCount: {
+			type: Number,
+			default: 6,
+		}, // maxCount makes sense when you have a restriction on the max quantity for a product
+		iconDimensions: {
+			type: Number,
+			default: 15,
+		},
+		minusIconFillColor: {
+			type: String,
+			default: '#000',
+		},
+		plusIconFillColor: {
+			type: String,
+			default: '#000',
+		},
+		isCountEditable: {
+			type: Boolean,
+			default: true,
+		},
+	},
+	data() {
+		return {
+			countData: this.count,
+		};
+	},
+	computed: {
+		minusIconColor: function() {
+			return this.countData === this.count ? '#CCC' : this.minusIconFillColor;
+		},
+		plusIconColor: function() {
+			return this.countData === this.maxCount ? '#CCC' : this.plusIconFillColor;
+		},
+	},
+	methods: {
+		decrement: function() {
+			if (this.countData > this.count) {
+				this.countData -= 1;
+			}
+		},
+		increment: function() {
+			if (this.countData < this.maxCount) {
+				this.countData += 1;
+			}
+		},
+		adjustCount: function() {
+			if (this.countData > this.maxCount) {
+				this.countData = this.maxCount;
+			} else if (this.countData < this.count) {
+				this.countData = this.count;
+			} else {
+				if (isNaN(Number(this.countData))) {
+					this.countData = this.count;
+				}
+			}
+		},
+	},
 };
 </script>
 
 <style scoped>
-    .nugget-quantity-counter {
-        display: inline-flex;
-    }
-    .nugget-quantity-counter div:first-child {
-        border: solid 1px #ccc;
-        border-radius: 5px 0px 0px 5px;
-    }
-    /*
-        We need the following commented CSS only if we want to replace the input box with a read only div
-    */
-    /* .nugget-quantity-counter div:nth-child(2) {
-        border-top: solid 1px #ccc;
-        border-bottom: solid 1px #ccc;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-    } */
-    .nugget-quantity-counter input[type="text"] {
-        border-top: solid 1px #CCC;
-        border-bottom: solid 1px #CCC;
-        border-left: none;
-        border-right: none;
-        text-align: center;
-        width: 20px;
-        padding: 12px;
-        margin: 0;
-        font-size: 16px;
-    }
-    .nugget-quantity-counter div:last-child {
-        border: solid 1px #ccc;
-        border-radius: 0px 5px 5px 0px;
-    }
-    .nugget-quantity-counter > div {
-        cursor: pointer;
-        padding: 12px;
-        width: 20px;
-        text-align: center;
-    }
-    .nugget-quantity-counter > div > svg {
-        height: 100%;
-    }
+.nugget-quantity-counter {
+	display: inline-flex;
+}
+.nugget-quantity-counter div:first-child {
+	border: solid 1px #ccc;
+	border-radius: 5px 0px 0px 5px;
+}
+.nugget-quantity-counter div:nth-child(2) {
+	border-top: solid 1px #ccc;
+	border-bottom: solid 1px #ccc;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+}
+.nugget-quantity-counter input[type='text'] {
+	border-top: solid 1px #ccc;
+	border-bottom: solid 1px #ccc;
+	border-left: none;
+	border-right: none;
+	text-align: center;
+	width: 20px;
+	padding: 12px;
+	margin: 0;
+	font-size: 16px;
+}
+.nugget-quantity-counter div:last-child {
+	border: solid 1px #ccc;
+	border-radius: 0px 5px 5px 0px;
+}
+.nugget-quantity-counter > div {
+	cursor: pointer;
+	padding: 12px;
+	width: 20px;
+	text-align: center;
+}
+.nugget-quantity-counter > div > svg {
+	height: 100%;
+}
 </style>
